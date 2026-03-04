4b7d282ec8f745dd04af667b82711a603818521e
aria00a <aria.00a@gmail.com>
2026-03-05 00:20:21 +0700

    coba drive 3

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const LANG = {
 
     // ── DATA ANALYST ───────────────────────────
     da_role: "Data Analyst",
-    da_name: "BBBBAria Bimantara",
+    da_name: "Aria Bimantara",
     da_about: "I am a detail-oriented Quality Assurance professional with over 5+ years of experience in the banking sector, currently transitioning into Data Analytics. With strong foundation in accuracy, risk assessment, and structured documentation across multiple system release cycles. Experienced in managing testing data, ensuring compliance with banking standards, and performing in-depth test analysis to reduce post-release defects and enhance system stability. Proficient in SQL for data extraction and analysis, with hands-on experience using Python and Tableau in portfolio projects to analyze datasets, identify trends, and develop interactive dashboards. Committed to applying analytical thinking and continuous learning to deliver reliable, data-driven business decisions.",
     da_cv_ats: "Download CV",
-    da_cv_ats_url: "#",
+    da_cv_ats_url: "https://drive.google.com/file/d/112ZK_ry7gHnTywV19FBgFqjMhl8-mIRw/view?usp=drive_link",
     da_edu_title: "Education",
     da_edu: [
       {
@@ -534,7 +534,7 @@ function render() {
 
   // Profile photo
   const photoMap = {
-    da: 'https://drive.google.com/uc?export=view&id=1MPbCmSQAaT9h9b8GKOXZsaVk_aHnrvP8',
+    da: 'img/Pas-Foto.jpg',
     qa: 'https://placehold.co/200x300/e6eaf0/192841?text=Photo',
   };
   document.getElementById('profile-photo').src = photoMap[p];
